f2ba15ff690e589873e6d4e6db36638bcbc7ab12
avocadohooman <gerhard@minimumbadass.com>
2021-09-18 12:35:48 +0300

    adding product controller/service/module

--- nestjs-boilerplate/src/main.ts
+++ nestjs-boilerplate/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(`Server running on ${PORT}`);
+  await app.listen(PORT);
 }
+
 bootstrap();
---
+++ nestjs-boilerplate/src/products/products.model.ts
@@ -0,0 +1,7 @@
+export class Product {
+	constructor(
+		public id: string, 
+		public title: string, 
+		public description: string, 
+		public price: number) {}
+}
